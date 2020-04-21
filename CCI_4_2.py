9831c776371e99c939c6cf2dd401eee18fd3c3d8
Johanna Kinsler <johanna.kinsler@gmail.com>
2020-04-21 22:40:05 +0000

    completed CCI problem 4.2

--- CCI_4_2.py
+++ CCI_4_2.py
@@ -46,10 +46,13 @@ class BinaryNode():
                 to_print.append(current.left)
                 to_print.append(current.right)
 
-def mid(arr):
-    """Finds the mid point in an array and return it"""
 
-    """pseudo code"""
+"""My solution is not optimized for tree height"""
+def mid(arr):
+    """Finds the mid point in an array and return it
+
+    Solution by JK"""
+
     
     arr_ln = len(arr)
     mid = arr_ln//2
@@ -73,32 +76,69 @@ def make_tree(arr):
             binary.insert(val)
 
     return binary
+"""END JK solution"""
 
 
+"""The CCI solution is optimized for tree height."""
+def min_height(srt_arr):
+    """CCI solution for problem 4.2 in Python"""
+
+    # Leave the recursive loop if the srt_arr is empty 
+    if len(srt_arr) == 0:
+        return None
+    
+    # find the center of the array
+    else: 
+        mid = len(srt_arr)//2 
+        
+        # set the center point in the middle or first middle node of the array
+        center = srt_arr[mid]
+
+        #create a new BinaryTree from the center point
+        tree = BinaryNode(center)
+        
+        # recurse on the left side of the array
+        tree.left = min_height(srt_arr[:(mid)])
+
+        # recurse on the right side of the array
+        tree.right = min_height(srt_arr[(mid+1):])
+
+        return tree
+"""END CCI solution"""
 
 
 if __name__ == '__main__':
 
-    """check that class and insert method are working.
+    """# check that class and insert method are working.
     node_test = BinaryNode(10)
     node_test.insert(5)
     node_test.insert(3)
     node_test.insert(8)
 
-    node_test.print_bi_tree()"""
+    node_test.print_bi_tree()
+    """
 
+    """# test the 'mid' funciton
     arr = [10, 20, 30, 40, 50, 60, 70, 80]
     print(f'\n\narray: {arr}')
 
     print(f'array pop order: ')
     for val in range(len(arr)):
         print(mid(arr))
+    """
 
+    """# test my solution"""
+    # print(f'test make_tree:')
+    # tree = make_tree(arr_2)
+    # print(tree)
+    # tree.print_bi_tree()
+
+    # test the CCI solution
     arr_2 = [10, 20, 30, 40, 50, 60, 70, 80]
-    print(f'test make_tree:')
-    tree = make_tree(arr_2)
-    print(tree)
-    tree.print_bi_tree()
+    print(f'arr_2: {arr_2}')
+    binary_tree = min_height(arr_2)
+    print(f'binary_tree from arr_2:')
+    binary_tree.print_bi_tree()
 
 
 
